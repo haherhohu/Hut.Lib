9cde5a64aff4884437967b756157cfd7ea042de1
Daegung Kim <haherhohu@hanmail.net>
2020-04-23 17:00:53 +0900

    remove nuget key(...)

--- Hut.Lib.Core/package.sh
+++
@@ -1,5 +0,0 @@
- # packaging sample script with nuget.package 
- # if using, change version (1.0.0)
- 
- dotnet pack
- dotnet nuget push /bin/Debug/Hut.Lib.1.0.0.nupkg -k oy2mwz4koqdyge3pg5fqagjdmzpvcxgezm43smmtn2dj7y -s https://api.nuget.org/v3/index.json
