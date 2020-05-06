f8274150e51b5cbb325e272ea50d822a157a0a50
Daegung Kim <haherhohu@hanmail.net>
2020-05-06 22:19:49 +0900

    update parser

--- Hut.Lib.Core/Parser/HutGeneralParser.cs
+++ Hut.Lib.Core/Parser/HutGeneralParser.cs
@@ -18,35 +18,35 @@ namespace Hut
     {
         protected object objects;
 
-        public const string ASTEROID_PROVISIONAL_DESIGNATION_REGEX = @"[0-9A-Z]{1}[0-9]{3}[\s]*[A-Z][A-Z\-]*[A-Z0-9][0-9]*"; // 1948 OA
+        //public const string ASTEROID_PROVISIONAL_DESIGNATION_REGEX = @"[0-9A-Z]{1}[0-9]{3}[\s]*[A-Z][A-Z\-]*[A-Z0-9][0-9]*"; // 1948 OA
 
         public HutGeneralParser(string filename = null)
         {
             objects = new object();
 
             if (filename != null)
-                readFile(filename);
+                Read(filename);
         }
 
-        public abstract void readFile(string filename);
+        public abstract void Read(string filename);
 
         public void Clear()
         {
             ((List<object>)objects).Clear();
         }
 
-        public int getNumberOfObjects()
+        public int NumberOfObjects()
         {
             return ((List<object>)objects).Count;
         }
 
-        public object[] getObjectArray()
+        public object[] ObjectArray()
         {
             return ((List<object>)objects).ToArray();
         }
 
         // check regular expression
-        protected string findExpression(string token, string regulerexpression)
+        protected string FindExpression(string token, string regulerexpression)
         {
             string result = null;
             Regex regex = new Regex(regulerexpression);
@@ -58,45 +58,45 @@ namespace Hut
             return result;
         }
 
-        protected string removeSpaces(string token, string replace = @" ")
+        protected string RemoveSpaces(string token, string replace = @" ")
         {
             return Regex.Replace(token, @"\s+", replace);
         }
 
-        protected string[] splitTextBySpaces(string text)
+        protected string[] SplitTextBySpaces(string text)
         {
-            return removeSpaces(text).Split(' ');
+            return RemoveSpaces(text).Split(' ');
         }
 
-        public static string convertFormattedHMS(string time)
+        public static string ConvertFormattedHMS(string time)
         {
             string hour = time.Substring(0, 2);
             string minute = time.Substring(2, 2);
             string second = time.Substring(4, 2);
 
-            return convertFormattedHMS(hour, minute, second);
+            return ConvertFormattedHMS(hour, minute, second);
         }
 
-        public static string convertFormattedYMD(string date)
+        public static string ConvertFormattedYMD(string date)
         {
             string year = date.Substring(0, 4);
             string month = date.Substring(4, 2);
             string day = date.Substring(6, 2);
 
-            return convertFormattedYMD(year, month, day);
+            return ConvertFormattedYMD(year, month, day);
         }
 
-        public static string convertFormattedHMS(string hour, string minute, string second)
+        public static string ConvertFormattedHMS(string hour, string minute, string second)
         {
-            return convertDateTimeFormat(hour, minute, second, @":");
+            return ConvertDateTimeFormat(hour, minute, second, @":");
         }
 
-        public static string convertFormattedYMD(string year, string month, string day)
+        public static string ConvertFormattedYMD(string year, string month, string day)
         {
-            return convertDateTimeFormat(year, month, day, @"-");
+            return ConvertDateTimeFormat(year, month, day, @"-");
         }
 
-        protected static string convertDateTimeFormat(string s1, string s2, string s3, string connector)
+        protected static string ConvertDateTimeFormat(string s1, string s2, string s3, string connector)
         {
             return (s1 + connector + s2 + connector + s3);
         }
